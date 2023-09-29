7890543fd7dca63bf6c7048661ba199f5da70ccf
marcopifferi <marcopifferi@users.noreply.github.com>
2023-09-29 17:19:02 +0200

    fix versions

--- core/src/glassfy.ts
+++ core/src/glassfy.ts
@@ -42,7 +42,7 @@ export class Glassfy {
     apiKey: string,
     watcherMode: boolean
   ): Promise<void> {
-    const version = '1.5.2';
+    const version = '1.5.3';
     return GlassfyModule.initialize(apiKey, watcherMode, version);
   }
 
