4a9502352ab69286dcf709ecbffe058dde701773
Luca Garbolino <387293+lgarbo@users.noreply.github.com>
2023-12-07 17:05:35 +0100

    update GlassfyModule to subclass NSObject

--- core/ios/GlassfyModule.h
+++ core/ios/GlassfyModule.h
@@ -1,6 +1,5 @@
 #import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
 
-@interface GlassfyModule : RCTEventEmitter <RCTBridgeModule>
+@interface GlassfyModule : NSObject <RCTBridgeModule>
 
 @end
